acf5f6774f79d28906fb5e1f72f9829f26d8fb2e
Miguel Ángel Rodríguez Álvarez Icaza <mike.rai@phiveleven.com>
2014-11-05 17:36:24 -0600

    Enhancements

--- minimalistic-experiment/js/scripts.js
+++ minimalistic-experiment/js/scripts.js
@@ -1,14 +1,26 @@
-$('a[href*=#]:not([href=#])').click(function() {
+$('a[href*=#]:not([href=#])').click(function(e) {
+    e.preventDefault();
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
         || location.hostname == this.hostname) {
-
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
            if (target.length) {
              $('html,body').animate({
                  scrollTop: target.offset().top-64
             }, 1000);
-            return false;
         }
     }
+});
+
+$(function(){
+    var d = $('#main');
+    var dPosTop = d.offset().top+10;
+    var win = $(window);
+    win.scroll(function(e){
+        var scrollTop = win.scrollTop();
+        if(scrollTop <= dPosTop){
+          d.show(669);
+        }
+    });
+
 });
