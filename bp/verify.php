7a9a8113fd62e4336608ba218b79d07f6da17ebd
Miguel Angel Rodriguez Alvarez Icaza <mike.rai@phiveleven.com>
2014-11-17 11:29:06 -0600

    Changes

--- bp/verify.php
+++ bp/verify.php
@@ -1,18 +1,5 @@
 <?php
-  require_once('recaptchalib.php');
-  $privatekey = "6LfP2P0SAAAAAFknptB7OPVeJTchzX7bALhMSMQs";
-  $resp = recaptcha_check_answer ($privatekey,
-                                $_SERVER["REMOTE_ADDR"],
-                                $_POST["recaptcha_challenge_field"],
-                                $_POST["recaptcha_response_field"]);
-
-  if (!$resp->is_valid) {
-    // What happens when the CAPTCHA was entered incorrectly
-    die ("The reCAPTCHA wasn't entered correctly. Go back and try it again." .
-         "(reCAPTCHA said: " . $resp->error . ")");
-  } else {
-    // Your code here to handle a successful verification
-    $email_to = "miguel@mikerai.com";
+  $email_to = "miguel@mikerai.com";
 	$name = $_POST["name"];
 	$email = $_POST["email"];
 	$message = $_POST["message"];
@@ -23,5 +10,4 @@
 	$headers .= "Content-type:text/html; charset=utf-8" . "\r\n"; 
 	$headers .= "From: <$email>" . "\r\n";
 	mail($email_to, "Message", $text, $headers);
-  }
 ?>
