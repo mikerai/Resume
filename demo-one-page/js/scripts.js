3f9f869efe238f27bd51e000db81482edf3391dd
Miguel Angel Rodriguez Alvarez Icaza <mike.rai@phiveleven.com>
2015-02-23 10:04:36 -0600

    Changes and experiments

--- demo-one-page/js/scripts.js
+++ demo-one-page/js/scripts.js
@@ -1,26 +1,39 @@
 //Custom scripts
 
+$(function () {
+    if ($(window).width() < 642) {
+     $('body').addClass('mobile');
+  }
+ 	else {
+    $('body').removeClass('mobile');
+  }
+});
+
+$(window).resize(function () {
+    if ($(window).width() < 642) {
+     $('body').addClass('mobile');
+  }
+ 	else {
+    $('body').removeClass('mobile');
+  }
+});
+
 $("input.menuB").on("click", function(e){
     e.preventDefault();
-    $('nav.mobile').slideToggle();
+    $('.mobile nav').slideToggle();
+});
+
+$('section, footer').on("click", function(e){
+	e.preventDefault();	
+	$('.mobile nav').slideUp();
 });
 
 // SPY MENU Cache selectors
-
-$(window).resize(function () {
-    if ($(window).width() < 640) {
-     $('nav').addClass('mobile');
-  }
- 	else {
-    $('nav').removeClass('mobile');
- }
-});
 
 $('nav a').click(function(){
     $('html, body').animate({
         scrollTop: $( $(this).attr('href') ).offset().top-64
     }, 987);
-    $('nav.mobile').slideToggle();
+    $('.mobile nav').slideToggle();
     return false;
 });
-
