8d216866aed39a6bfcac6045bd284422b56f1552
Miguel Ángel Rodríguez Álvarez Icaza <mike.rai@phiveleven.com>
2015-07-21 14:50:40 -0500

    Recalculated scroll

--- resume_mid_2015-5.5.2/js/scripts.js
+++ resume_mid_2015-5.5.2/js/scripts.js
@@ -137,8 +137,8 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
+          scrollTop: target.offset().top-$('header').height()
+        }, 1597);
         return false;
       }
     }
