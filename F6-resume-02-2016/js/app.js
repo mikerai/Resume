4f43c0c417edfddd5ba8c320d179167838292133
Miguel Ángel Rodríguez Álvarez Icaza <mike.rai@phiveleven.com>
2016-03-02 16:46:33 -0600

    Minor improvements... almost nothing changed

--- F6-resume-02-2016/js/app.js
+++ F6-resume-02-2016/js/app.js
@@ -25,21 +25,6 @@ $(function(){
 	  }
 	}
 
-	$(function() {
-	  $('a[href*="#"]:not([href="#"])').click(function() {
-	    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
-	      var target = $(this.hash);
-	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-	      if (target.length) {
-	        $('html, body').animate({
-	          scrollTop: target.offset().top -60
-	        }, 1000);
-	        return false;
-	      }
-	    }
-	  });
-	});
-
   if (isMobile()) {
     $('.linkedin, .cd-top').remove();
   }
@@ -132,3 +117,43 @@ $(function(){
   });
 
 });
+
+$(document).ready(function () {
+  $(document).on("scroll", onScroll);
+  
+  //smoothscroll
+  $('a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+      $(document).off("scroll");
+      
+      $('a').each(function () {
+          $(this).removeClass('active');
+      });
+      $(this).addClass('active');
+    
+      var target = this.hash,
+          menu = target;
+      $target = $(target);
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top-60
+      }, 1000, 'swing', function () {
+          window.location.hash = target;
+          $(document).on("scroll", onScroll);
+      });
+  });
+});
+
+function onScroll(event){
+  var scrollPos = $(document).scrollTop();
+  $('#ha-header nav a').each(function () {
+      var currLink = $(this);
+      var refElement = $(currLink.attr("href"));
+      if (refElement.position().top-62 <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+          $('#ha-header nav a').removeClass("active");
+          currLink.addClass("active");
+      }
+      else{
+          currLink.removeClass("active");
+      }
+  });
+}
