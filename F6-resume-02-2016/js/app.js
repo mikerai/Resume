ad574680855ce3b59487a11dd9e2e9b8fcffc572
Miguel Ángel Rodríguez Álvarez Icaza <mike.rai@phiveleven.com>
2016-03-08 15:21:00 -0600

    Changes

--- F6-resume-02-2016/js/app.js
+++ F6-resume-02-2016/js/app.js
@@ -82,7 +82,7 @@ $(function(){
 
   // ---- Back to top desktop and tablet 
 
-  jQuery(document).ready(function($){
+  /*jQuery(document).ready(function($){
     // browser window scroll (in pixels) after which the "back to top" link is shown
     var offset = 400,
     //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
@@ -108,7 +108,7 @@ $(function(){
         }, scroll_top_duration
       );
     });
-  });
+  });*/
 
   //---- Back to top mobile
 
@@ -156,4 +156,69 @@ function onScroll(event){
           currLink.removeClass("active");
       }
   });
+}
+
+// video js
+
+$( document ).ready(function() {
+
+    scaleVideoContainer();
+
+    initBannerVideoSize('.video-container .poster img');
+    initBannerVideoSize('.video-container .filter');
+    initBannerVideoSize('.video-container video');
+
+    $(window).on('resize', function() {
+        scaleVideoContainer();
+        scaleBannerVideoSize('.video-container .poster img');
+        scaleBannerVideoSize('.video-container .filter');
+        scaleBannerVideoSize('.video-container video');
+    });
+
+});
+
+function scaleVideoContainer() {
+
+    var height = $(window).height() + 5;
+    var unitHeight = parseInt(height) + 'px';
+    $('.homepage-hero-module').css('height',unitHeight);
+
+}
+
+function initBannerVideoSize(element){
+
+    $(element).each(function(){
+        $(this).data('height', $(this).height());
+        $(this).data('width', $(this).width());
+    });
+
+    scaleBannerVideoSize(element);
+
+}
+
+function scaleBannerVideoSize(element){
+
+    var windowWidth = $(window).width(),
+    windowHeight = $(window).height() + 5,
+    videoWidth,
+    videoHeight;
+
+    console.log(windowHeight);
+
+    $(element).each(function(){
+        var videoAspectRatio = $(this).data('height')/$(this).data('width');
+
+        $(this).width(windowWidth);
+
+        if(windowWidth < 1000){
+            videoHeight = windowHeight;
+            videoWidth = videoHeight / videoAspectRatio;
+            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+
+            $(this).width(videoWidth).height(videoHeight);
+        }
+
+        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+
+    });
 }
