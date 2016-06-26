4ad91324c508ed44ed33b91e29644e2d94cff796
Miguel Angel Rodriguez Alvarez Icaza <mike.rai@phiveleven.com>
2016-06-26 02:04:18 -0500

    Live version uploaded

--- sara_junio_16/js/app.js
+++ sara_junio_16/js/app.js
@@ -1,1 +1,21 @@
 $(document).foundation();
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top-44
+        }, 1000);
+        //$(this).foundation('close');
+        return false;
+      }
+    }
+  });
+});
+
+$('.off-canvas a').on('click', function() {
+    $('.off-canvas').foundation('close');
+});
--- sara_junio_16/js/min/app-min.js
+++ sara_junio_16/js/min/app-min.js
@@ -1,2 +1,2 @@
-$(document).foundation();
+$(document).foundation(),$(function(){$('a[href*="#"]:not([href="#"])').click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var n=$(this.hash);if(n=n.length?n:$("[name="+this.hash.slice(1)+"]"),n.length)return $("html, body").animate({scrollTop:n.offset().top-44},1e3),!1}})}),$(".off-canvas a").on("click",function(){$(".off-canvas").foundation("close")});
 //# sourceMappingURL=./app-min.js.map
