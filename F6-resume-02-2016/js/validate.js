543be92293885bb271a81f8c0788f7c863c9bf65
Miguel Angel Rodriguez Alvarez Icaza <mike.rai@phiveleven.com>
2016-07-19 00:46:25 -0500

    Contact form is working... need to minify and concatenate

--- F6-resume-02-2016/js/validate.js
+++ F6-resume-02-2016/js/validate.js
@@ -1,24 +1,30 @@
 $(document).ready(function() {
-		$("#submit").click(function() {
-			var name = $("#name").val();
-			var email = $("#email").val();
-			var message = $("#message").val();
-				$("#returnmessage").empty(); // To empty previous error/success message.
-				// Checking for blank fields.
-				if (name === '' || email === '' || message === '') {
-					alert("Please fill required fields");
-				} else {
-				// Returns successful data submission message when the entered information is stored in database.
-				$.post("mailer.php", {
-					name1: name,
-					email1: email,
-					message1: message
-					}, function(data) {
-						$("#returnmessage").append(data); // Append returned message to message paragraph.
-						if (data === "Your query has been received, I will contact you soon.") {
-							$("#form")[0].reset(); // To reset form fields on success.
-						}
-				});
-			}
-		});
+  $("#submit").click(function() {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+  $("#returnmessage").empty(); // To empty previous error/success message.
+    // Checking for blank fields.
+    if (name == '' || email == '') {
+      alert("Please complete all the fields");
+    } else {
+  // Returns successful data submission message when the entered information is stored in database.
+    $.post("mailer.php", {
+      name1: name,
+      email1: email,
+      message1: message
+    }, function(data) {
+      $("#returnmessage").append(data); // Append returned message to message paragraph.
+        if (data == "Your message has arrived, I will contact you as soon as possible.") {
+          $("#form")[0].reset(); // To reset form fields on success.
+          $("p.message").addClass("success");
+          $("p.message").removeClass("error");
+        }
+        if (data == "Please use only letters." || data == "<span>* Invalid email address *</span>") {
+          $("p.message").addClass("error");
+          $("p.message").removeClass("success");
+        }
+      });
+    }
+  });
 });
--- F6-resume-02-2016/mailer.php
+++ F6-resume-02-2016/mailer.php
@@ -3,32 +3,40 @@
 $name = $_POST['name1'];
 $email = $_POST['email1'];
 $message = $_POST['message1'];
+$reg = "/^[a-zA-Z\x{0080}-\x{024F}\s\/\-\)\(\`\.\"\']+$/u";
 $email = filter_var($email, FILTER_SANITIZE_EMAIL); // Sanitizing E-mail.
 // After sanitization Validation is performed
-	if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-		else {
-			$subject = $name;
-			// To send HTML mail, the Content-type header must be set.
-			$headers = 'MIME-Version: 1.0' . "\r\n";
-			$headers = 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-			$headers = 'From:' . $email. "\r\n"; // Sender's Email
-			$headers = 'Cc:' . $email. "\r\n"; // Carbon copy to Sender
-			$template = '<div style="padding:50px; color:white;">Hello ' . $name . ',<br/>'
-			. '<br/>Thank you...! For Contacting Us.<br/><br/>'
-			. 'Name:' . $name . '<br/>'
-			. 'Email:' . $email . '<br/>'
-			. 'Message:' . $message . '<br/><br/>'
-			. 'This is a Contact Confirmation mail.'
-			. '<br/>'
-			. 'We Will contact You as soon as possible .</div>';
-			$sendmessage = "<div style=\"background-color:#7E7E7E; color:white;\">" . $template . "</div>";
-			// Message lines should not exceed 70 characters (PHP rule), so wrap it.
-			$sendmessage = wordwrap($sendmessage, 70);
-			// Send mail by PHP Mail Function.
-			mail("m@mikerai.com", $subject, $sendmessage, $headers);
-			echo "Your query has been received, I will contact you soon.";
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		if (!preg_match($reg, $name)) {
+				echo "Please use only letters.";
+		} else {
+				$subject = $name;
+				// To send HTML mail, the Content-type header must be set.
+				$to = "m@mikerai.com";
+				$headers = 'MIME-Version: 1.0' . "\r\n";
+				$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+				$headers .= 'From:' . $email. "\r\n"; // Sender's Email
+				$headers .= 'Cc:' . $email. "\r\n"; // Carbon copy to Sender
+				$template = '<div style="padding:50px; color:#8a8a8a;">Hello ' . $name . ',<br/>'
+				. '<br/>Thank you...! For Contacting Us.<br/><br/>'
+				. 'Name: ' . $name . '<br/>'
+				. 'Email: ' . '&nbsp;' . $email . '<br/>'
+				. 'Message: ' . $message . '<br/><br/>'
+				. 'This is a Contact Confirmation mail.'
+				. '<br/>'
+				. 'We Will contact You as soon as possible.</div>';
+				$sendmessage = "<div style=\"background-color:#ffffff; color:#8a8a8a;\">" . $template . "</div>";
+				// Message lines should not exceed 70 characters (PHP rule), so wrap it.
+				$sendmessage = wordwrap($sendmessage, 70);
+				// Send mail by PHP Mail Function.
+				if ( mail($to, $subject, $sendmessage, $headers)){
+					echo "Your message has arrived, I will contact you as soon as possible.";
+				}
+				else{
+					echo "error sending mail";
+				}
 		}
-	} else {
+} else {
 	echo "<span>* Invalid email *</span>";
-	}
+}
 ?>
